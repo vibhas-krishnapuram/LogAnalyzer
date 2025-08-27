52997e0db5df3a7af51939d2870c88129154e1ce
VKrishit123 <vibhask08@gmail.com>
2025-08-27 13:25:41 -0400

    Geo IP added

---
+++ geoIP.py
@@ -0,0 +1,39 @@
+import requests
+
+logs = [
+        {'date': '2025-08-25T08:12:41', 'host': 'ubuntu', 'process': 'sshd', 'pid': '1350', 'status': 'Accepted', 'user': 'alice', 'ip': '192.168.1.101', 'port': '50321', 'protocol': 'ssh2'}, 
+        {'date': '2025-08-25T08:13:02', 'host': 'ubuntu', 'process': 'sshd', 'pid': '1352', 'status': 'Failed', 'user': 'bob', 'ip': '192.168.1.105', 'port': '60411', 'protocol': 'ssh2'}, 
+        {'date': '2025-08-25T08:13:04', 'host': 'ubuntu', 'process': 'sshd', 'pid': '1352', 'status': 'Failed', 'user': 'bob', 'ip': '192.168.1.105', 'port': '60411', 'protocol': 'ssh2'}, 
+        {'date': '2025-08-25T08:13:07', 'host': 'ubuntu', 'process': 'sshd', 'pid': '1352', 'status': 'Failed', 'user': 'bob', 'ip': '192.168.1.105', 'port': '60411', 'protocol': 'ssh2'}, 
+        {'date': '2025-08-25T08:13:10', 'host': 'ubuntu', 'process': 'sshd', 'pid': '1352', 'status': 'Failed', 'user': 'bob', 'ip': '192.168.1.105', 'port': '60411', 'protocol': 'ssh2'}, 
+        {'date': '2025-08-25T08:13:12', 'host': 'ubuntu', 'process': 'sshd', 'pid': '1352', 'status': 'Failed', 'user': 'bob', 'ip': '103.102.220.12', 'port': '60411', 'protocol': 'ssh2'}, 
+        {'date': '2025-08-25T08:13:13', 'host': 'ubuntu', 'process': 'sshd', 'pid': '1352', 'status': 'Failed', 'user': 'bob', 'ip': '192.168.1.105', 'port': '60411', 'protocol': 'ssh2'}, 
+        {'date': '2025-08-25T08:16:10', 'host': 'ubuntu', 'process': 'sshd', 'pid': '1362', 'status': 'Accepted', 'user': 'charlie', 'ip': '192.168.1.102', 'port': '51001', 'protocol': 'ssh2'}, 
+        {'date': '2025-08-25T08:17:42', 'host': 'ubuntu', 'process': 'sshd', 'pid': '1365', 'status': 'Failed', 'user': 'root', 'ip': '185.199.110.20', 'port': '33321', 'protocol': 'ssh2'}, 
+        {'date': '2025-08-25T08:17:44', 'host': 'ubuntu', 'process': 'sshd', 'pid': '1365', 'status': 'Failed', 'user': 'root', 'ip': '185.199.110.20', 'port': '33321', 'protocol': 'ssh2'},
+        {'date': '2025-08-25T08:17:48', 'host': 'ubuntu', 'process': 'sshd', 'pid': '1365', 'status': 'Failed', 'user': 'root', 'ip': '103.102.220.5', 'port': '33321', 'protocol': 'ssh2'},
+        {'date': '2025-08-25T08:17:48', 'host': 'ubuntu', 'process': 'sshd', 'pid': '1365', 'status': 'Failed', 'user': 'root', 'ip': '8.8.8.8', 'port': '33321', 'protocol': 'ssh2'}
+]
+
+
+def geo_IP(ip):
+    allowed = ['US', 'CA', 'Unknown']
+    try:
+        res = requests.get(f"https://ipinfo.io/{ip}/json").json()
+        if res.get("country", "Unknown") in allowed:
+            return "Allowed"
+        else:
+            alert = {
+                "ip": ip,
+                "country": res.get("country"),
+                "alert": "Country is not on allowed list"
+            }
+            return alert
+    except:
+        return "Failed"
+
+
+for log in logs:
+    ip = log['ip']
+    country = geo_IP(ip)
+    print(country)
