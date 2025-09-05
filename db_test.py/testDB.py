a2a7a6f28f832b003ac7683e04c7568552df143a
VKrishit123 <vibhask08@gmail.com>
2025-09-05 17:27:18 -0400

    testing db

---
+++ db_test.py/testDB.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, flash, redirect, url_for, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite://users.sqlite3"
+
+
+db = SQLAlchemy(app)
+
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+
+if __name__ == '__main__':
+        app.run(debug=True)
