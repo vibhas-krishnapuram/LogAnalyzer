babecade4d63e2bec5556df270b644d6097f08d3
VKrishit123 <vibhask08@gmail.com>
2025-09-07 11:37:19 -0400

    formatting

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/home", methods=["GET", "POST"])
 def upload_file():
     if request.method == 'POST':
         # Check if a file was uploaded
